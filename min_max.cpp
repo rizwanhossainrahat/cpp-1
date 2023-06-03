24254a9d8fde5bf330dd23fdc9b05627ff809ecb
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2023-06-04 02:34:35 +0600

    first commit

---
+++ min_max.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int mn=min(a,b);
+    int mx=max(a,b);
+    cout<<mn <<" "<<mx<<endl;
+    
+    return 0;
+}
---
+++ output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // std::cout<<"hello\n";
+    // std::cout<<"World\n";
+    // std::cout<<"hello "<<"world";
+    // std::cout<<"hello "<<std::endl <<"world";
+    int a=100; int b=200;
+    // std::cout<<a;
+    cout<<a<<" "<<b<<endl;
+
+}
